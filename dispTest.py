fefe89da70b29f44022acd9ec893dcd4ad2cb396
nisaak <nisaak@student.ethz.ch>
2019-04-17 10:17:37 +0200

    structuring code and adding video support

--- dispTest.py
+++ dispTest.py
@@ -9,103 +9,230 @@ Created on Wed Apr  3 16:04:16 2019
 
 import cv2
 import numpy as np
-import feat_match
-import rectification_uncalib
-import rectification_calib
-import matplotlib
-
+import matplotlib.image
+from uvdisparity import uv_disp
 #specify path
-path_left = '/home/nisaak/Documents/semester_thesis/export_test/day_cloudy1/left/frame0076.jpg'
-path_right = '/home/nisaak/Documents/semester_thesis/export_test/day_cloudy1/right/frame0077.jpg'
+path_left = '/home/nisaak/Documents/semester_thesis/export_test/day_cloudy1/left/frame0067.jpg'
+path_right = '/home/nisaak/Documents/semester_thesis/export_test/day_cloudy1/right/frame0068.jpg'
+
+video = 1
 
 
 
 
 #load images
-img_left = cv2.imread(path_left,0)
-img_left = cv2.resize(img_left, None, None,fx=0.5,fy=0.5,interpolation = cv2.INTER_AREA)
-BLACK = [0,0,0]
-img_left = np.array(img_left, dtype= np.uint8)
-cv2.imshow('left',img_left)
-img_right = cv2.imread(path_right,0)
-img_right = cv2.resize(img_right,None, None, fx=0.5,fy=0.5,interpolation = cv2.INTER_AREA)
+
+def load_images(path_right, path_left):
+    
+    #load images and save as grayscale
+    
+    img_left = cv2.imread(path_left,0)
+    img_right = cv2.imread(path_right,0)
+    
+    #rescale images
+    
+    img_left = cv2.resize(img_left, None, None,fx=0.5,fy=0.5,interpolation = cv2.INTER_AREA) 
+    img_right = cv2.resize(img_right,None, None, fx=0.5,fy=0.5,interpolation = cv2.INTER_AREA)
+
+    #convert to uint8
+    img_left = np.array(img_left, dtype= np.uint8)
+    img_right = np.array(img_right,dtype=np.uint8)
+    
+    
+    return img_right, img_left
 
 
-img_right = np.array(img_right,dtype=np.uint8)
+def rectify_and_remap(img_right, img_left):
+    
 
-R_left = np.matrix('0.99934 0.000099 -0.036288; 0.000041 0.999993 0.003857; 0.036288 -0.003856 0.999334',np.float32)
-R_right = np.matrix('0.998947 0.001982 -0.045833; -0.002159 0.999990 -0.003809; 0.045825 0.003904 0.998942',np.float32)
-#M = np.matrix('1.053655553328680439e+00 -9.217688345630560554e-03 -1.312381196971389663e+01; \
-#              9.701598541577670376e-03 1.019989885839541088e+00 -3.065585312977083632e+01; \
-#              2.927936696573258345e-05 3.003417193742115971e-05 1.000000000000000000e+00')
-#img_right_undis, img_left_undis = rectification_calib.rect_calib(img_right, img_left)
+    
+    
+    #rectification matrices
+    
+    R_left = np.array([[0.99934, 0.000099, -0.036288],\
+                      [0.000041, 0.999993, 0.003857],\
+                      [0.036288, -0.003856, 0.999334]],\
+                       np.float32)
+    
+    R_right = np.array([[0.998947, 0.001982, -0.045833],\
+                        [-0.002159, 0.999990, -0.003809],\
+                        [0.045825, 0.003904, 0.998942]],\
+                        np.float32)
+    
+    #camera matrices
+    
+    cam_mat_left = np.matrix([[351.087640, 0.000000, 337.599686],\
+                             [0.000000, 351.004496, 193.493580],\
+                             [0.000000, 0.000000, 1.000000]],\
+                             np.float32)
+    
+    cam_mat_right = np.matrix([[358.601746, 0.000000, 335.011174],\
+                              [0.000000, 358.378270, 178.059030],\
+                              [0.000000, 0.000000, 1.000000]],\
+                              np.float32)
+    
+    #distortion coefficients
+    
+    dist_left = np.array([-0.166992, 0.031731, 0.002833, 0.000323, 0.000000],np.float32)
+    
+    dist_right  = np.array([-0.156551, 0.018860, 0.002804, -0.001476, 0.000000],np.float32)
+    
+    
+    
+    """"
+    these are the parameters from the official calibration file
+    
+    ///////
+    cam_mat_left = np.matrix('350.409 0.00 338.233; 0.0 350.409 193.477; 0.0 0.0 1.0',np.float32)
+    dist_left= np.matrix('-0.174318 0.0261121 0.002833 0.000323 0.0',np.float32)
+    cam_mat_right = np.matrix('349.38 0.00 335.541;0.0 349.38 195.064; 0.0 0.0 1.0',np.float32)
+    dist_right = np.matrix('-0.172731 0.0257849 0.002804 -0.001476 0.0',np.float32)
+    
+    M = np.matrix('1.053655553328680439e+00 -9.217688345630560554e-03 -1.312381196971389663e+01; \
+                  9.701598541577670376e-03 1.019989885839541088e+00 -3.065585312977083632e+01; \
+                  2.927936696573258345e-05 3.003417193742115971e-05 1.000000000000000000e+00')
+    img_right_undis, img_left_undis = rectification_calib.rect_calib(img_right, img_left)
+    
+    ///////
+    
+    """
+    
+    #find remap matrices
+    
+    m1_left, m2_left = cv2.initUndistortRectifyMap(cam_mat_left, dist_left, R_left,None,(img_left.shape[1],img_left.shape[0]), cv2.CV_32FC1)
+    
+    m1_right, m2_right = cv2.initUndistortRectifyMap(cam_mat_right, dist_right, R_right,None,(img_left.shape[1],img_left.shape[0]), cv2.CV_32FC1)
+    
+    #remap
+    
+    img_right_undis = cv2.remap(img_right, m1_right,m2_right, cv2.INTER_CUBIC)
+    
+    img_left_undis = cv2.remap(img_left,m1_left,m2_left,cv2.INTER_CUBIC)
+    
+    #img_right_undis = cv2.normalize(img_right_undis, None, alpha=0, beta = 200, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_8UC1)
+    #img_left_undis = cv2.normalize(img_left_undis, None, alpha=0, beta = 200, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_8UC1)
+    
+    return img_right_undis, img_left_undis
 
-cam_mat_left = np.matrix('351.087640 0.000000 337.599686; 0.000000 351.004496 193.493580; 0.000000 0.000000 1.000000',np.float32)
-dist_left = np.matrix('-0.166992 0.031731 0.002833 0.000323 0.000000',np.float32)
-cam_mat_right = np.matrix('358.601746 0.000000 335.011174; 0.000000 358.378270 178.059030; 0.000000 0.000000 1.000000',np.float32)
-dist_right  = np.matrix('-0.156551 0.018860 0.002804 -0.001476 0.000000',np.float32)
-
-m1_left, m2_left = cv2.initUndistortRectifyMap(cam_mat_left, dist_left, R_left,None,(img_left.shape[1],img_left.shape[0]), cv2.CV_32FC1)
-m1_right, m2_right = cv2.initUndistortRectifyMap(cam_mat_right, dist_right, R_right,None,(img_right.shape[1],img_left.shape[0]), cv2.CV_32FC1)
-
-img_right_undis = cv2.remap(img_right, m1_right,m2_right, cv2.INTER_NEAREST)
-img_left_undis = cv2.remap(img_left,m1_left,m2_left,cv2.INTER_NEAREST)
-
-img_left_undis = cv2.copyMakeBorder(img_left_undis, 0,0,400,400, cv2.BORDER_CONSTANT, value = [255,255,255])
-
-img_right_undis = cv2.copyMakeBorder(img_right_undis, 0,0,400,400, cv2.BORDER_CONSTANT, value = [255,255,255])
 
 
-cv2.imshow('img_r', img_right_undis)
-cv2.imshow('img_l',img_left_undis)
+
+
+
+
+
+def disparity(img_right_undis, img_left_undis):
+    
+    
+    #workaround for trunctuation bug, create border with sufficient size around image
+    
+    img_left_undis = cv2.copyMakeBorder(img_left_undis, 0,0,400,400, cv2.BORDER_CONSTANT, value = [255,255,255])
+
+    img_right_undis = cv2.copyMakeBorder(img_right_undis, 0,0,400,400, cv2.BORDER_CONSTANT, value = [255,255,255])
+    
+    
 ######
-window_size = 9
-minD = -21
-numD = 16*13
-blockS = 5
-#####
-stereo = cv2.StereoSGBM_create(minDisparity = minD,\
-                               numDisparities=numD,\
-                               blockSize=blockS,\
-                               P1 = 8*window_size**2,\
-                               P2 = 32*window_size**2,\
-                               disp12MaxDiff = 12, \
-                               preFilterCap = 63,\
-                               uniquenessRatio = 7,\
-                               speckleWindowSize = 0,\
-                               speckleRange = 2)
-"""
-minDisparity = minD,\
-                               numDisparities=numD,\
-                               blockSize=blockS,P1 = 600,\
-                               P2 = 2400,\
-                               disp12MaxDiff = 10, \
-                               preFilterCap = 4,\
-                               uniquenessRatio = 1,\
-                               speckleWindowSize = 150,\
-                               speckleRange = 2)
-"""
+    window_size = 11
+    minD = -10
+    numD = 16*5
+    blockS = 9
+    #####
+    stereo = cv2.StereoSGBM_create(minDisparity = minD,\
+                                   numDisparities=numD,\
+                                   blockSize=blockS,\
+                                   P1 = 8*window_size**2,\
+                                   P2 = 32*window_size**2,\
+                                   disp12MaxDiff = 12, \
+                                   preFilterCap = 30,\
+                                   uniquenessRatio = 1,\
+                                   speckleWindowSize = 20,\
+                                   speckleRange = 2)
+    """
+    ######
+    window_size = 7
+    minD = -20
+    numD = 16*10
+    blockS = 3
+    #####
+    stereo = cv2.StereoSGBM_create(minDisparity = minD,\
+                                   numDisparities=numD,\
+                                   blockSize=blockS,\
+                                   P1 = 8*window_size**2,\
+                                   P2 = 32*window_size**2,\
+                                   disp12MaxDiff = 12, \
+                                   preFilterCap = 30,\
+                                   uniquenessRatio = 1,\
+                                   speckleWindowSize = 50,\
+                                   speckleRange = 1)
+    """
+    
+    disparity = stereo.compute(img_left_undis, img_right_undis, None)
+    disparity = disparity[:,400:(disparity.shape[1]-400)]
+    disparity = cv2.normalize(disparity, None,alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+    disparity_coloured = cv2.applyColorMap(disparity, cv2.COLORMAP_HSV)
+    
+    return disparity, disparity_coloured
 
-#M, _, _, _ = feat_match.match(img_right_undis, img_left_undis)
-#
-#img_left_warped = cv2.warpPerspective(img_left_rect, M, (img_left_rect.shape[1], img_left_rect.shape[0]))
-#cv2.imshow('Left',img_left_warped)
-#
-#t_x = img_right.shape[1]/2.0
-#t_y = img_right.shape[0]/2.0
-#
-#image_corr = np.array([[1,0,t_x],[0,1,t_y]],np.float32)
-#img_right_undis = cv2.warpAffine(img_right_undis, image_corr, (img_right.shape[1],img_right.shape[0]))
-#cv2.imshow('aff',img_right_undis)
-#img_left_undis = cv2.warpAffine(img_left_undis, image_corr, (img_left.shape[1],img_left.shape[0]))
-#
-disparity = stereo.compute(img_left_undis, img_right_undis, None)
-disparity = disparity[:,400:(disparity.shape[1]-400)]
-disparity = cv2.normalize(disparity, None,alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
-disparity = cv2.applyColorMap(disparity, cv2.COLORMAP_HSV)
-depth  = np.zeros_like(disparity)
+if video == 1:
+#specify video loading
+    video_left = '/home/nisaak/Documents/semester_thesis/export_test/dübendorf/left/left.avi'
+    video_right = '/home/nisaak/Documents/semester_thesis/export_test/dübendorf/right/right.avi'
+    
+    
+    names = [video_left,\
+             video_right];
+    window_titles = ['left', 'right']
+    
+    cap = [cv2.VideoCapture(i) for i in names]
+    
+    frames = [None] * len(names);
+    gray = [None] * len(names);
+    ret = [None] * len(names);
+    
+    while True:
+    
+        for i,c in enumerate(cap):
+            if c is not None:
+                ret[i], frames[i] = c.read();
+                
+        for i,f in enumerate(frames):
+            if ret[i] is True:
+                gray[i] = cv2.cvtColor(f, cv2.COLOR_BGR2GRAY)
+                cv2.imshow(window_titles[i], gray[i]);
+      
+                
+                
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+           break
+       
+        img_right = cv2.equalizeHist(np.copy(gray[1]))
+        img_left = cv2.equalizeHist(np.copy(gray[0]))
+
+        img_left = cv2.resize(img_left, None, None,fx=0.5,fy=0.5,interpolation = cv2.INTER_AREA) 
+        img_right = cv2.resize(img_right,None, None, fx=0.5,fy=0.5,interpolation = cv2.INTER_AREA)
+
+        undis_right, undis_left = rectify_and_remap(img_right, img_left)
+        
+        disparity_img, _ = disparity(undis_right, undis_left)
+        cv2.imshow('disp', disparity_img)
+        uv_disp(disparity_img, img_right)
+        
+    
+    for c in cap:
+        if c is not None:
+            c.release();
+else:
+    img_right, img_left = load_images(path_right, path_left)
+    undis_right, undis_left = rectify_and_remap(img_right, img_left)
+    disparity, _ = disparity(undis_right, undis_left)
+    cv2.imshow('disp', disparity)   
+    cv2.waitKey()     
 
 
-cv2.imshow('disp', disparity)
-cv2.waitKey()
+cv2.waitKey(10)
 cv2.destroyAllWindows()
+
+
+
+
+
--- uvdisparity.py
+++ uvdisparity.py
@@ -5,188 +5,205 @@ import numpy as np
 import math
 
 
-
-path = "/home/nisaak/Documents/semester_thesis/code_test/test_pictures/"
-#imgpath = path  + "Cars_gnd.png"
-imgpath = path  + "disp_kitti.png"
-#imgpath = path  + "disp.jpg"
-#imgpath = path + "test_2.png"
-
-
-#
-img = np.asarray(cv2.imread(imgpath,cv2.IMREAD_GRAYSCALE))
-img = cv2.resize(img, (420,289), interpolation =cv2.INTER_AREA)
-#cv2.imshow('img', img)
-#    cv2.waitKey()
-(img_height, img_width) = np.shape(img)
-print(img_height,img_width)
-
-
-MAX_DISPARITY = 200 
-MIN_DISPARITY = 0
-#    def u_disparity(self):
-U_disp = np.zeros((MAX_DISPARITY, img_width),np.float)
-for u in range(img_width):
-    U_disp[..., u] = cv2.calcHist(images = [img[..., u]],channels = [0],mask = None, histSize = [MAX_DISPARITY], 
-                                  ranges = [0, MAX_DISPARITY]).flatten() / np.float(img_width)
-uhist_vis = np.array(U_disp*255, np.uint8)
-ublack_mask = uhist_vis < 30
-uhist_vis[ublack_mask] = 0
-U_disp = uhist_vis
-#MAX_U = np.max(U_disp)
-#U_disp = np.multiply(U_disp, 1./MAX_U)
-
-#ret,U_disp = cv2.threshold(U_disp,0.1,1,cv2.THRESH_BINARY)
-#U_disp = cv2.applyColorMap(U_disp,cv2.COLORMAP_JET)
-cv2.imshow('U_disp', U_disp)
-
-#ret, labels = cv2.connectedComponents(img = U_disp, connectivity = 8)
-#cv2.imshow('labels',labels)
-
-
-
-#    MAX2 = np.max(U_disp)
-
+def uv_disp(disp, img_right):
+        
+    """
+    path = "/home/nisaak/Documents/semester_thesis/code_test/test_pictures/"
+    #imgpath = path  + "Cars_gnd.png"
+    imgpath = path  + "disparity.png"
+    #imgpath = path  + "disp.jpg"
+    #imgpath = path + "test_2.png"
     
-#    def v_disparity(self, image):
-V_disp = np.zeros((img_height, MAX_DISPARITY),np.float)
-for v in range(img_height):
-    V_disp[v, ...] = cv2.calcHist(images = [img[v, ...]],channels = [0],mask = None, histSize = [MAX_DISPARITY], 
-                                  ranges = [0, MAX_DISPARITY]).flatten() / np.float(img_height)
-
-#MAX_V = np.max(V_disp)
-#V_disp = V_disp/MAX_V
-#ret,V_disp = cv2.threshold(V_disp,0.25,1,cv2.THRESH_BINARY)
-vhist_vis  = np.array(V_disp*255, np.uint8)
-vblack_mask = vhist_vis < 20
-vhist_vis[vblack_mask] = 0
-V_disp = vhist_vis
- 
-cv2.imshow('V_disp', V_disp)
-#cv2.waitKey()
-
-V_disp = V_disp.astype(np.uint8)
-U_disp = U_disp.astype(np.uint8)
-
-#convert V_disp to right format
-cdst = cv2.cvtColor(V_disp, cv2.COLOR_GRAY2BGR)
-
-#convert U_disp to right format
-cdst_u = cv2.cvtColor(U_disp, cv2.COLOR_GRAY2BGR)
-
-#linedetection v disp
-v_lines = cv2.HoughLinesP(V_disp, 1, np.pi/180, 100, None, 100, 3) 
-#TODO make houghdetection more robust
-
-
-#lines can extend outside rectangle making gradient false
-threshold_line = 100
-for i in range(0, len(v_lines)):
-    l = v_lines[i][0]
-#    if l[3]-l[1] > threshold_line:
-#    angle = math.atan(math.fabs((l[2]-l[1])/(l[3]-l[0])))
-#    if (angle > 280*np.pi/180 and angle < -10*np.pi/180):
-    cv2.line(cdst, (l[0], l[1]), (l[2], l[3]), (255,255,0), 1, cv2.LINE_AA)
-
-
-"""
-a,b,c = v_lines.shape
-for i in range(a):
-#    if v_lines[i][0][0] > (-np.pi/2 + 10*np.pi/180) and v_lines[i][0][0] < (-10*np.pi/180):
-    rho = v_lines[i][0][0]
-    theta = v_lines[i][0][1]
-    a = math.cos(theta)
-    b = math.sin(theta)
-    x0, y0 = a*rho, b*rho
-    pt1 = ( int(x0+1000*(-b)), int(y0+1000*(a)) )
-    pt2 = ( int(x0-1000*(-b)), int(y0-1000*(a)) )
-    cv2.line(cdst,pt1,pt2,(255,255,255),2, cv2.LINE_AA)
-"""
-
-cdst = cv2.cvtColor(cdst, cv2.COLOR_BGR2GRAY)
-#cv2.imshow('cdst', cdst)
-
-
-
-"""
-#linedetection udisp
-u_lines = cv2.HoughLinesP(U_disp, 1, np.pi/180, 10,None,10,20) 
-
-for i in range(0, len(u_lines)):
-    l = u_lines[i][0]
-    cv2.line(cdst_u, (l[0], l[1]), (l[2], l[3]), (255,255,0), 1, cv2.LINE_AA)
-
-   
-cdst_u = cv2.cvtColor(cdst_u, cv2.COLOR_BGR2GRAY)
-cv2.imshow('cdst_u',cdst_u)
-"""
-#create mask for image
-v_line = np.zeros((img_height,1))
-for n in range(np.uint8(img_height/2),img_height):
-    v_line[n] = np.argmax(cdst[n,:])
-
-#    v_lines[n,0] = np.argwhere(cdst[n,:]).max(1)
-#print(line)
-threshold = 7
-
-lower = np.uint8(img_height/2)
-#calculate mask for v disparity
-mask_v_disp = np.zeros((img_height,img_width),np.uint8)
-for m in range(lower,img_height):
-    for n in range(img_width):
-        if (img[m,n] > (v_line[m]-threshold) and img[m,n] < (v_line[m]+threshold)):
-            mask_v_disp[m,n] = 255
-            img[m,n]=0
-        else:
-            mask_v_disp[m,n] = 0
-imgandmask = cv2.applyColorMap(img, cv2.COLORMAP_HSV)
-mask_v_disp = cv2.cvtColor(mask_v_disp, cv2.COLOR_GRAY2BGR)
-imgandmask = cv2.addWeighted(imgandmask,0.5,mask_v_disp,0.5,1)
-"""
-
-mask_u_disp = np.zeros((img_height,img_width))
-
-u_line = np.zeros((1,img_width),np.uint8)
-try:
-    for n in range((img_width)):
-        u_line[0,n] = np.max(np.nonzero(cdst_u[:,n]))
-except ValueError:
-    pass
-#    u_line[0,n] = np.argwhere(cdst_u[:,n]).max(0)
-threshold_obj = 5
-
-
-
-
-
-#calculate mask for u disparity add disparity colour
-mask_u_disp = np.zeros((img_height,img_width))
-
-for n in range(img_width):
+    
+    path_left = '/home/nisaak/Documents/semester_thesis/export_test/day_cloudy1/left/frame0161.jpg'
+    source_img = np.uint8(cv2.imread(path_left, cv2.IMREAD_GRAYSCALE))
+    source_img = cv2.resize(source_img, (420,289), interpolation =cv2.INTER_CUBIC)
+    """
+    source_img = disp
+    source_img = cv2.resize(source_img, (420,289), interpolation =cv2.INTER_CUBIC)
+    print(source_img.shape)
+    
+    
+    #
+    #img = np.asarray(cv2.imread(imgpath,cv2.IMREAD_GRAYSCALE))
+    img = img_right
+    img = cv2.resize(img, (420,289), interpolation =cv2.INTER_CUBIC)
+    #cv2.imshow('img', img)
+    #    cv2.waitKey()
+    (img_height, img_width) = np.shape(img)
+    print(img_height,img_width)
+    
+    
+    MAX_DISPARITY = 200 
+    MIN_DISPARITY = 0
+    #    def u_disparity(self):
+    U_disp = np.zeros((MAX_DISPARITY, img_width),np.float)
+    for u in range(img_width):
+        U_disp[..., u] = cv2.calcHist(images = [img[..., u]],channels = [0],mask = None, histSize = [MAX_DISPARITY], 
+                                      ranges = [0, MAX_DISPARITY]).flatten() / np.float(img_width)
+    uhist_vis = np.array(U_disp*255, np.uint8)
+    ublack_mask = uhist_vis < 30
+    uhist_vis[ublack_mask] = 0
+    U_disp = uhist_vis
+    #MAX_U = np.max(U_disp)
+    #U_disp = np.multiply(U_disp, 1./MAX_U)
+    
+    #ret,U_disp = cv2.threshold(U_disp,0.1,1,cv2.THRESH_BINARY)
+    #U_disp = cv2.applyColorMap(U_disp,cv2.COLORMAP_JET)
+    cv2.imshow('U_disp', U_disp)
+    
+    #ret, labels = cv2.connectedComponents(img = U_disp, connectivity = 8)
+    #cv2.imshow('labels',labels)
+    
+    
+    
+    #    MAX2 = np.max(U_disp)
+    
+        
+    #    def v_disparity(self, image):
+    V_disp = np.zeros((img_height, MAX_DISPARITY),np.float)
+    for v in range(img_height):
+        V_disp[v, ...] = cv2.calcHist(images = [img[v, ...]],channels = [0],mask = None, histSize = [MAX_DISPARITY], 
+                                      ranges = [0, MAX_DISPARITY]).flatten() / np.float(img_height)
+    
+    #MAX_V = np.max(V_disp)
+    #V_disp = V_disp/MAX_V
+    #ret,V_disp = cv2.threshold(V_disp,0.25,1,cv2.THRESH_BINARY)
+    vhist_vis  = np.array(V_disp*255, np.uint8)
+    vblack_mask = vhist_vis < 20
+    vhist_vis[vblack_mask] = 0
+    V_disp = vhist_vis
+     
+    cv2.imshow('V_disp', V_disp)
+    #cv2.waitKey()
+    
+    V_disp = V_disp.astype(np.uint8)
+    U_disp = U_disp.astype(np.uint8)
+    
+    #convert V_disp to right format
+    cdst = cv2.cvtColor(V_disp, cv2.COLOR_GRAY2BGR)
+    
+    #convert U_disp to right format
+    cdst_u = cv2.cvtColor(U_disp, cv2.COLOR_GRAY2BGR)
+    
+    #linedetection v disp
+    v_lines = cv2.HoughLinesP(V_disp, 1, np.pi/180, 100, None, 100, 3) 
+    #TODO make houghdetection more robust
+    
+    
+    #lines can extend outside rectangle making gradient false
+    threshold_line = 100
+    if v_lines: 
+        for i in range(0, len(v_lines)):
+            l = v_lines[i][0]
+        #    if l[3]-l[1] > threshold_line:
+        #    angle = math.atan(math.fabs((l[2]-l[1])/(l[3]-l[0])))
+        #    if (angle > 280*np.pi/180 and angle < -10*np.pi/180):
+            cv2.line(cdst, (l[0], l[1]), (l[2], l[3]), (255,255,0), 1, cv2.LINE_AA)
+    else:
+        print("no lines detected")
+    
+    
+    """
+    a,b,c = v_lines.shape
+    for i in range(a):
+    #    if v_lines[i][0][0] > (-np.pi/2 + 10*np.pi/180) and v_lines[i][0][0] < (-10*np.pi/180):
+        rho = v_lines[i][0][0]
+        theta = v_lines[i][0][1]
+        a = math.cos(theta)
+        b = math.sin(theta)
+        x0, y0 = a*rho, b*rho
+        pt1 = ( int(x0+1000*(-b)), int(y0+1000*(a)) )
+        pt2 = ( int(x0-1000*(-b)), int(y0-1000*(a)) )
+        cv2.line(cdst,pt1,pt2,(255,255,255),2, cv2.LINE_AA)
+    """
+    
+    cdst = cv2.cvtColor(cdst, cv2.COLOR_BGR2GRAY)
+    #cv2.imshow('cdst', cdst)
+    
+    
+    
+    """
+    #linedetection udisp
+    u_lines = cv2.HoughLinesP(U_disp, 1, np.pi/180, 10,None,10,20) 
+    
+    for i in range(0, len(u_lines)):
+        l = u_lines[i][0]
+        cv2.line(cdst_u, (l[0], l[1]), (l[2], l[3]), (255,255,0), 1, cv2.LINE_AA)
+    
+       
+    cdst_u = cv2.cvtColor(cdst_u, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('cdst_u',cdst_u)
+    """
+    #create mask for image
+    v_line = np.zeros((img_height,1))
+    for n in range(np.uint8(img_height/2),img_height):
+        v_line[n] = np.argmax(cdst[n,:])
+    
+    #    v_lines[n,0] = np.argwhere(cdst[n,:]).max(1)
+    #print(line)
+    threshold = 7
+    
+    lower =0 
+    #np.uint8(img_height/2)
+    #calculate mask for v disparity
+    mask_v_disp = np.zeros((img_height,img_width),np.uint8)
     for m in range(img_height):
-        if img[m,n] > 5:
-            if img[m,n] < (u_line[0,m]-threshold_obj) and img[m,n] > (u_line[0,m]+threshold_obj):
-                mask_u_disp[m,n] = 255
+        for n in range(img_width):
+            if (img[m,n] > (v_line[m]-threshold) and img[m,n] < (v_line[m]+threshold)):
+                mask_v_disp[m,n] = 255
+    #            img[m,n]=0
             else:
-                mask_u_disp[m,n] = 0
-#            if (np.any(np.where(cdst_u[:,n] > 0))-threshold_obj) >= img[m,n] <= (np.any(np.where(cdst_u[:,n] > 0))+threshold_obj):
-#                    mask_u_disp[m,n] = 1
-#            else:
-#                mask_u_disp[m,n] = 0
-
-"""
-
-#matplotlib.image.imsave('img',img)
-#matplotlib.image.imsave('v_disp',V_disp)
-#matplotlib.image.imsave('v_line',cdst)
-#matplotlib.image.imsave('u_disp',U_disp)
-#matplotlib.image.imsave('u_line',cdst_u)
-#cv2.imshow('mask',mask_v_disp)
-#cv2.imshow('mask_u',mask_u_disp)
-
-cv2.imshow('img',img)
-
-cv2.waitKey()
-#print(type(V_disp))
-#print(type(v_lines))
-#cv2.imwrite(cdst, u_lines)
+                mask_v_disp[m,n] = 0
+    imgandmask = cv2.applyColorMap(img, cv2.COLORMAP_HSV)
+    #mask_v_disp = cv2.cvtColor(mask_v_disp, cv2.COLOR_GRAY2BGR)
+    print(mask_v_disp.shape)
+    imgandmask = cv2.addWeighted(source_img,0.5,mask_v_disp,0.5,1)
+    """
+    
+    mask_u_disp = np.zeros((img_height,img_width))
+    
+    u_line = np.zeros((1,img_width),np.uint8)
+    try:
+        for n in range((img_width)):
+            u_line[0,n] = np.max(np.nonzero(cdst_u[:,n]))
+    except ValueError:
+        pass
+    #    u_line[0,n] = np.argwhere(cdst_u[:,n]).max(0)
+    threshold_obj = 5
+    
+    
+    
+    
+    
+    #calculate mask for u disparity add disparity colour
+    mask_u_disp = np.zeros((img_height,img_width))
+    
+    for n in range(img_width):
+        for m in range(img_height):
+            if img[m,n] > 5:
+                if img[m,n] < (u_line[0,m]-threshold_obj) and img[m,n] > (u_line[0,m]+threshold_obj):
+                    mask_u_disp[m,n] = 255
+                else:
+                    mask_u_disp[m,n] = 0
+    #            if (np.any(np.where(cdst_u[:,n] > 0))-threshold_obj) >= img[m,n] <= (np.any(np.where(cdst_u[:,n] > 0))+threshold_obj):
+    #                    mask_u_disp[m,n] = 1
+    #            else:
+    #                mask_u_disp[m,n] = 0
+    
+    """
+    
+    #matplotlib.image.imsave('img',imgandmask)
+    #matplotlib.image.imsave('v_disp',V_disp)
+    #matplotlib.image.imsave('v_line',cdst)
+    #matplotlib.image.imsave('u_disp',U_disp)
+    #matplotlib.image.imsave('u_line',cdst_u)
+    cv2.imshow('mask',imgandmask)
+    #cv2.imshow('mask_u',mask_u_disp)
+    
+    cv2.imshow('img',img)
+    
+    cv2.waitKey()
+    #print(type(V_disp))
+    #print(type(v_lines))
+    #cv2.imwrite(cdst, u_lines)
