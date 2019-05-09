77c199bba7aea60a70e35eaa295dd158115348f6
nisaak <nisaak@student.ethz.ch>
2019-05-09 19:00:11 +0200

    update

--- code/uv_disp.py
+++ code/uv_disp.py
@@ -13,234 +13,264 @@ MAX_DISPARITY = 255
 
 #def dir_filter():
     
+class UV_disp:
 
-
-def load_and_resize(disp_map, source_image):
-    
-    width, height = disp_map.shape
-
-
-    src_img = np.copy(source_image)
-    disp = np.copy(disp_map)
-    
-    src_img = cv2.resize(src_img, (width, height), interpolation =cv2.INTER_CUBIC)
-    disp = cv2.resize(disp, (width, height), interpolation =cv2.INTER_CUBIC)
-
-    return disp, src_img
-
-def u_disp(disparity):
-    
-    height, width = disparity.shape
-    
-    #specify image shape for later use
-    
-    disp_map = np.copy(disparity).astype(np.uint8)
-    
-    
-    
-    #create U disparity histogram
-    
-    U_disp = np.zeros((MAX_DISPARITY, width),np.float)
-    for u in range(width):
-        U_disp[..., u] = cv2.calcHist(images = [disp_map[..., u]],channels = [0],mask = None, histSize = [MAX_DISPARITY], 
-                                      ranges = [0, MAX_DISPARITY]).flatten() / np.float(width)
-    
-    uhist_vis = np.array(U_disp*255, np.uint8) #scale
-    ublack_mask = uhist_vis < 5    #define threshold for black mask
-    uhist_vis[ublack_mask] = 0  #set black mask to zero
-
-    U_disp = uhist_vis
-    
-#    U_disp = cv2.normalize(U_disp, U_disp, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-    
-    return U_disp
-            
-def v_disp(disparity):
-    
-    height, width = disparity.shape
-    
-    
-
-
-    disp_map = np.copy(disparity)
-
-    
-
-    
-    #    def v_disparity(self, image):
-    V_disp = np.zeros((height, MAX_DISPARITY),np.float)
-    for v in range(height):
-        V_disp[v, ...] = cv2.calcHist(images = [disp_map[v, ...]],channels = [0],mask = None, histSize = [MAX_DISPARITY], 
-                                      ranges = [0, MAX_DISPARITY]).flatten() / np.float(height)
-
-    vhist_vis  = np.array(V_disp*255, np.uint8)
-    vblack_mask = vhist_vis < 10
-    vhist_vis[vblack_mask] = 0
-    V_disp = vhist_vis
-    
-#    V_disp = cv2.normalize(V_disp, V_disp, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
-    
-    return V_disp
-
-def u_hough(U_disp, method):
-    
-    
-    
-    #convert V_disp to right format
-    cdst = np.zeros_like(U_disp)   
-    threshold  = 150
-    
-    #specify which method to use, probabilistic or standard hough
-
-    if method == 0:
+    def load_and_resize(self,disp_map, source_image):
         
-        u_lines = cv2.HoughLines(U_disp, 2, np.pi/180, threshold)
-        
-        a,b,c = u_lines.shape
-        for i in range(a):
-            #if v_lines[i][0][0] > (-np.pi/2 + 10*np.pi/180) and v_lines[i][0][0] < (-10*np.pi/180):
-            rho = u_lines[i][0][0]
-            theta = u_lines[i][0][1]
-            a = math.cos(theta)
-            b = math.sin(theta)
-            x0, y0 = a*rho, b*rho
-            pt1 = ( int(x0+1000*(-b)), int(y0+1000*(a)) )
-            pt2 = ( int(x0-1000*(-b)), int(y0-1000*(a)) )
-            cv2.line(cdst,pt1,pt2,(255,255,255),1, cv2.LINE_AA)
+        width, height = disp_map.shape
     
     
-    if method == 1:
-        u_lines = cv2.HoughLinesP(U_disp, 2, np.pi/180, threshold, None, 30, 15) 
+        src_img = np.copy(source_image)
+        disp = np.copy(disp_map)
         
-        #lines can extend outside rectangle making gradient false
+        src_img = cv2.resize(src_img, (self.width, self.height), interpolation =cv2.INTER_CUBIC)
+        disp = cv2.resize(disp, (self.width, self.height), interpolation =cv2.INTER_CUBIC)
+    
+        return disp, src_img
+    
+    def u_disp(self,disparity):
+        
+        height, width = disparity.shape
+        
+        #specify image shape for later use
+        
+        disp_map = np.copy(disparity).astype(np.uint8)
+        
+        
+        
+        #create U disparity histogram
+        
+        U_disp = np.zeros((MAX_DISPARITY, width),np.float)
+        for u in range(width):
+            U_disp[..., u] = cv2.calcHist(images = [disp_map[..., u]],channels = [0],mask = None, histSize = [MAX_DISPARITY], 
+                                          ranges = [0, MAX_DISPARITY]).flatten() / np.float(width)
+        
+        uhist_vis = np.array(U_disp*255, np.uint8) #scale
+        ublack_mask = uhist_vis < 5    #define threshold for black mask
+        uhist_vis[ublack_mask] = 0  #set black mask to zero
+    
+        U_disp = uhist_vis
+        
+    #    U_disp = cv2.normalize(U_disp, U_disp, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+        
+        return U_disp
+                
+    def v_disp(self,disparity):
+        
+        height, width = disparity.shape
+        
+        
+    
+    
+        disp_map = np.copy(disparity).astype(np.uint8)
+    
+        
+    
+        
+        #    def v_disparity(self, image):
+        V_disp = np.zeros((height, MAX_DISPARITY),np.float)
+        for v in range(height):
+            V_disp[v, ...] = cv2.calcHist(images = [disp_map[v, ...]],channels = [0],mask = None, histSize = [MAX_DISPARITY], 
+                                          ranges = [0, MAX_DISPARITY]).flatten() / np.float(height)
+    
+        cv2.normalize(V_disp, V_disp, 0 ,255, cv2.NORM_MINMAX, cv2.CV_32F)
+#        V_disp = V_disp.astype(np.uint8)
+        vhist_vis  = np.array(V_disp*255, np.uint8)
+#        vblack_mask = vhist_vis < 5
+#        vhist_vis[vblack_mask] = 0
+        V_disp = vhist_vis
+        
+    #    V_disp = cv2.normalize(V_disp, V_disp, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+        
+        return V_disp
+    
+    def u_hough(self, U_disparity):
+        
+        
+        U_disp = np.copy(U_disparity)
+        U_disp[U_disp.shape[0]-5:U_disp.shape[0],:] = 0
+        U_disp[0:5,:] = 0
+
+        
+        #convert V_disp to right format
+        cdst_hor = np.zeros_like(U_disp)
+        max_linelen = 150
+        threshold_hor = 120
+        v_lines_hor = cv2.HoughLinesP(U_disp, 1, np.pi/180, threshold_hor, None, 30, 5) #maybe only use longest line
         try:
-            if u_lines.any:
-                for i in range(0, len(u_lines)):
-                    l = u_lines[i][0]
-                    cv2.line(cdst, (l[0], l[1]), (l[2], l[3]), (255,255,0), 1, cv2.LINE_8)
+            if v_lines_hor.any:
+                for i in range(0, len(v_lines_hor)): #iterate through all lines, find longest and print it
+                    l = v_lines_hor[i][0]
+                    if np.abs(l[2]-l[0]) < max_linelen and np.abs(l[3]-l[1]) < 5:
+                      cv2.line(cdst_hor, (l[0], l[1]), (l[2], l[3]), (255,255,0), 1, cv2.LINE_8)
             else:
-                print('no lines detected')
+              print('no lines detected')
         except AttributeError:
             print('no lines detected')
-
-#    cdst = cv2.cvtColor(cdst, cv2.COLOR_BGR2GRAY)
+                
+        return cdst_hor
     
-    return cdst
-
-    
-def v_hough(V_disp, method):
-    
-    
-    
-    V_disp[0:int(V_disp.shape[0]/3),:] = 0 #make upper third black for more relevant line detection
-    #convert V_disp to right format
-    cdst = np.zeros_like(V_disp)   
-    cdst_vert = np.zeros_like(V_disp)
-    threshold  = 30
-    #specify which method to use, probabilistic or standard hough
-
-    if method == 0:
         
-        v_lines = cv2.HoughLines(V_disp, 1, np.pi/180, threshold)
+    def v_hough(self,V_disparity):
         
-        a,b,c = v_lines.shape
-        for i in range(a):
-            #if v_lines[i][0][0] > (-np.pi/2 + 10*np.pi/180) and v_lines[i][0][0] < (-10*np.pi/180):
-            rho = v_lines[i][0][0]
-            theta = v_lines[i][0][1]
-            a = math.cos(theta)
-            b = math.sin(theta)
-            x0, y0 = a*rho, b*rho
-            pt1 = ( int(x0+1000*(-b)), int(y0+1000*(a)) )
-            pt2 = ( int(x0-1000*(-b)), int(y0-1000*(a)) )
-            cv2.line(cdst,pt1,pt2,(255,255,255),1, cv2.LINE_AA)
-    
-    
-    if method == 1:
-        v_lines = cv2.HoughLinesP(V_disp, 1, np.pi/180, threshold, None, 50, 50) #maybe only use longest line
-
+        V_disp = np.copy(V_disparity)
+        
+        V_disp[0:int(V_disp.shape[0]/3),:] = 0 #make upper third black for more relevant line detection
+        #convert V_disp to right format
+        cdst = np.zeros_like(V_disp)   
+        threshold_long  = 100
+        v_lines = cv2.HoughLinesP(V_disp, 1, np.pi/180, threshold_long, None, 50, 5) #maybe only use longest line
+        self.gradient = 0 #initialize gradient
         try:
             if v_lines.any:
                 max_dist= -1.0
                 max_l = np.zeros((4,1))
                 for i in range(0, len(v_lines)): #iterate through all lines, find longest and print it
                     l = v_lines[i][0]
-                    if np.abs(l[2]-l[0]) >= 10:
+                    if np.abs(l[2]-l[0]) >= 5:
                         theta1 = (l[3]-l[1])
                         theta2 = (l[2]-l[0])
                         hyp = math.hypot(theta1, theta2)
                         if max_dist < hyp:
                             max_l = l
                             max_dist = hyp
+                            self.gradient = (max_l[3]-max_l[1])/(max_l[0]-max_l[2])
                 cv2.line(cdst, (max_l[0], max_l[1]), (max_l[2], max_l[3]), (255,255,0), 1, cv2.LINE_8)
 
             else:
                 print('no lines detected')
         except AttributeError:
             print('no lines detected')
-            
-    cv2.imshow('cdst', cdst)
-
-#    cdst = cv2.cvtColor(cdst, cv2.COLOR_BGR2GRAY)
+                
+        cv2.imshow('cdst', cdst)
     
-    return cdst
-    
-def mask(disparity, cdst):
-    
-    
-    height, width = disparity.shape
-    
-    cdst = cv2.resize(cdst, dsize = (255, height))
-     
-    threshold = 12
-    
-    disp_map = np.copy(disparity)
-    
-    #create mask for image
-    
-    v_line = np.zeros((height,1))
-    for n in range(np.uint8(height/3),height):
-        v_line[n] = np.argmax(cdst[n,:]) #this might not be the best way to do the disparity comparision
-#        print(v_line[n])
-#        v_line[n,0] = np.argwhere(cdst[n,:]).max(1)
-    #calculate mask for v disparity
-
-    mask_v_disp = np.zeros((height,width),np.uint8)
-    for m in range(height):
-        threshold -=10/(2*height)
-        for n in range(width):
-
-
-            if v_line[m] == 0:
-                mask_v_disp[m,n] = 0
-
-            elif (disp_map[m,n]  > (v_line[m]-threshold) and disp_map[m,n] < (v_line[m]+threshold)):
-                mask_v_disp[m,n] = 255
+        return cdst
+      
+      
+    def v_hough_peaks(self, V_disparity):
+        V_disp = np.copy(V_disparity)
+        
+#        V_disp[0:int(V_disp.shape[0]/3),:] = 0 #make upper third black for more relevant line detection
+        #convert V_disp to right format
+        cdst_vert = np.zeros_like(V_disp)
+        max_linelen = 100
+        threshold_vert = 60
+        v_lines_vert = cv2.HoughLinesP(V_disp, 1, np.pi/180, threshold_vert, None, 50, 10) #maybe only use longest line
+        try:
+            if v_lines_vert.any:
+                for i in range(0, len(v_lines_vert)): #iterate through all lines, find longest and print it
+                    l = v_lines_vert[i][0]
+                    if np.abs((l[3]-l[1])) < max_linelen and np.abs(l[2]-l[0]) < 5:
+                      cv2.line(cdst_vert, (l[0], l[1]), (l[2], l[3]), (255,255,0), 1, cv2.LINE_8)
             else:
-                mask_v_disp[m,n] = 0
-
-   
+              print('no lines detected')
+        except AttributeError:
+            print('no lines detected')
+                
+        return cdst_vert
+      
+      
+    def mask(self,disparity, cdst):
         
-    return mask_v_disp
-    
-def refine_mask(mask):
-    new_mask = np.zeros_like(mask)
-    for val in np.unique(mask)[1:]:
-        mask_for_labels = np.uint8(mask == val)
-        labels, stats = cv2.connectedComponentsWithStats(mask_for_labels, 4)[1:3]
-        largest_label = 1 + np.argmax(stats[1:, cv2.CC_STAT_AREA])
-        new_mask[labels == largest_label] = val
         
-    kernel = np.ones((3,3), np.uint8)
-    kernel_close = np.ones((3,3), np.uint8)
-    opening = cv2.morphologyEx(new_mask, cv2.MORPH_OPEN, kernel)
-#    opening = cv2.morphologyEx(opening, cv2.MORPH_OPEN, kernel)
-    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel_close)
-    new_mask = np.copy(closing)
+        height, width = disparity.shape
+            
+        cdst = cv2.resize(cdst, dsize = (255, height))
+         
+        threshold_baseline = 15 #adjust threshold according to line gradient
+        grad = self.gradient
+#        print(self.gradient)
+        k = math.pi/2 - math.atan(grad) #gradient dependent threshold
+        threshold = math.cos(k) * threshold_baseline
+        
+        disp_map = np.copy(disparity)
+        
+        #create mask for image
+        
+        v_line = np.zeros((height,1))
+        for n in range(np.uint8(height/3),height):
+            v_line[n] = np.argmax(cdst[n,:]) #this might not be the best way to do the disparity comparision
+    #        v_line[n,0] = np.argwhere(cdst[n,:]).max(1)
+        #calculate mask for v disparity
     
-    return new_mask
+        mask_v_disp = np.zeros((height,width),np.uint8)
+        for m in range(height):
+            for n in range(width):   
+                if v_line[m] == 0:
+                    mask_v_disp[m,n] = 0
     
+                elif (disp_map[m,n]  >= (v_line[m]-threshold) and disp_map[m,n] <= (v_line[m]+threshold)):
+                    mask_v_disp[m,n] = 255
+                else:
+                    mask_v_disp[m,n] = 0
+    
+       
+            
+        return mask_v_disp
+      
+    def mask_obstacles(self,disparity, cdst):
+        
+        
+        height, width = disparity.shape
+            
+        cdst = cv2.resize(cdst, dsize = (width, 255))
 
+
+         
+        threshold = 5 #adjust threshold according to line gradient
+        
+        
+        disp_map = np.copy(disparity)
+        
+        #create mask for image
+        
+        u_line = np.zeros((1,width))
+        for n in range(width):
+#            u_line[0,n] = UV_disp.last_nonzero(cdst[:,n],0)
+            u_line[0,n] = (cdst[:,n]!=0).argmax(axis=0)
+            
+            
+        mask_obst = np.zeros((height,width),np.uint8)
+        for m in range(height):
+            for n in range(width):
+                if u_line[0,n] == 0:
+                    mask_obst[m,n] = 0
+                elif (disp_map[m,n]  >= (u_line[0,n]-threshold) and disp_map[m,n] <= (u_line[0,n]+threshold)):
+                    mask_obst[m,n] = 255
+                else:
+                    mask_obst[m,n] = 0
     
-    
-###IGNORE FOR NOW####
+       
+            
+        return mask_obst
+        
+    def refine_mask(self, mask):
+        new_mask = np.zeros_like(mask)
+        for val in np.unique(mask)[1:]:
+            mask_for_labels = np.uint8(mask == val)
+            labels, stats = cv2.connectedComponentsWithStats(mask_for_labels, 4)[1:3]
+            largest_label = 1 + np.argmax(stats[1:, cv2.CC_STAT_AREA])
+            new_mask[labels == largest_label] = val
+            
+        kernel = np.ones((3,3), np.uint8)
+        kernel_close = np.ones((3,3), np.uint8)
+        opening = cv2.morphologyEx(new_mask, cv2.MORPH_OPEN, kernel)
+        opening = cv2.morphologyEx(opening, cv2.MORPH_OPEN, kernel)
+        closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel_close)
+        new_mask = np.copy(closing)
+        
+        return new_mask
+        
+    def similarity_compute(self, cdst_v, cdst_u):
+        cv2.blur(cdst_v, (7,7))
+        cv2.blur(cdst_u, (7,7))
+        obst = np.matmul(cdst_v, cdst_u)
+        return obst*255
+      
+      
+    def last_nonzero(arr, axis, invalid_val=-1):
+      mask = arr!=0
+      val = arr.shape[axis] - np.flip(mask, axis=axis).argmax(axis=axis) - 1
+      return np.where(mask.any(axis=axis), val, invalid_val)
+        
+    ###IGNORE FOR NOW####
--- code/zed_runfile.py
+++ code/zed_runfile.py
@@ -7,6 +7,10 @@ import uv_disp
 import numpy as np
 import math
 
+
+
+
+
 camera_settings = sl.CAMERA_SETTINGS.CAMERA_SETTINGS_BRIGHTNESS
 str_camera_settings = "BRIGHTNESS"
 step_camera_settings = 1
@@ -18,9 +22,9 @@ def main():
     init = sl.InitParameters()
     init.camera_resolution = sl.RESOLUTION.RESOLUTION_VGA
     init.camera_fps = 60
-    init.depth_mode = sl.DEPTH_MODE.DEPTH_MODE_PERFORMANCE  # Use PERFORMANCE depth mode
+    init.depth_mode = sl.DEPTH_MODE.DEPTH_MODE_MEDIUM  # Use PERFORMANCE depth mode
     init.coordinate_units = sl.UNIT.UNIT_MILLIMETER  # Use milliliter units (for depth measurements)
-    init.depth_minimum_distance = 300
+    init.depth_minimum_distance = 150
     cam = sl.Camera()
     if not cam.is_opened():
         print("Opening ZED Camera...")
@@ -35,6 +39,9 @@ def main():
     mat = sl.Mat()
     disparity = sl.Mat()
     depth = sl.Mat()
+    fourcc = cv2.VideoWriter_fourcc('F','M','P','4')
+    video_file=cv2.VideoWriter('video.avi',fourcc, 10,(672, 376),0)
+
 
     print_camera_information(cam)
     print_help()
@@ -51,38 +58,59 @@ def main():
             
             
             ###MASK CALCULATIONS
-            
-            disp_np = np.copy(disparity.get_data()) #copy and convert to numpy array..! there are issues because normalization with nan and inf
-            info = np.finfo(disp_np.dtype)
-            disp_np[np.isnan(disp_np)] = 0 #this is need because else the images becomes black after normalization
-            disp_np[np.isinf(disp_np)] = 0
 
+            u = uv_disp.UV_disp()
+           
+            disp_np = np.copy(disparity.get_data()) #copy and convert to numpy array..! there are issues because normalization with nan and inf
+#            info = np.finfo(disp_np.dtype)
+#            disp_np[np.isnan(disp_np)] = 0 #this is need because else the images becomes black after normalization
+            disp_np[np.isinf(disp_np)] = 0
             cv2.normalize(disp_np, disp_np, 0, 255, cv2.NORM_MINMAX, cv2.CV_32F) #normalizing makes flickering artifacts
             disp_np = disp_np.astype(np.uint8)
-            V_disp = uv_disp.v_disp(disp_np) #compute v-disp histogram
-            
-            houghlines = uv_disp.v_hough(V_disp, 1)
-            disp_np = cv2.resize(disp_np, dsize =(0,0),fx = 0.5, fy= 0.5, interpolation = cv2.INTER_AREA)
 
+
+
+            """
+            TODO:
+            Currently, disparity map is normalized according to uint8 range, try to find a way to preserve original disparity map values and compare with them
+            """
+            V_disp = u.v_disp(disp_np) #compute v-disp histogram
+            U_disp = u.u_disp(disp_np)
             
-            floor_mask = uv_disp.mask(disp_np, houghlines)
-            floor_mask = cv2.resize(floor_mask, dsize =(0,0),fx = 2, fy= 2,interpolation = cv2.INTER_AREA)
             
-            refined_mask = uv_disp.refine_mask(floor_mask) #astype uint8
+            #maybe remove noise in disparity map
+            
+            v_houghlines = u.v_hough(V_disp)
+            u_houghlines = u.u_hough(U_disp)
+            disp_np = cv2.resize(disp_np, dsize =(0,0),fx = 0.25, fy= 0.25, interpolation = cv2.INTER_AREA)
+            
+            obst_mask = u.mask_obstacles(disp_np, u_houghlines)
+            
+            floor_mask = u.mask(disp_np, v_houghlines)
+            floor_mask = cv2.resize(floor_mask, dsize =(0,0),fx = 4, fy= 4,interpolation = cv2.INTER_AREA)
+            
+            refined_mask = u.refine_mask(floor_mask) #astype uint8
+#            refined_mask = cv2.cvtColor(refined_mask, cv2.COLOR_GRAY2BGR)
 
             cam_img = mat.get_data() #astype uint8 4 channels RGBA
             cam_img = cv2.cvtColor(cam_img, cv2.COLOR_RGBA2GRAY)
-            
-            
+#            cam_img = cv2.cvtColor(cam_img, cv2.COLOR_RGBA2BGR)
             mask_on_img = cv2.addWeighted(cam_img,1,refined_mask,0.5, 5)
+            disp_np = cv2.resize(disp_np, dsize= (0,0), fx=4, fy=4)
+            obst_mask = cv2.resize(obst_mask, dsize= (0,0), fx=4, fy=4)
+            obst_on_img = cv2.addWeighted(cam_img,1,obst_mask,0.5, 5)
 
 
-            
-            
-            disp_np = cv2.resize(disp_np, dsize= (0,0), fx=2, fy=2)
+                
+            video_file.write(mask_on_img)
+            cv2.imshow('disp',disp_np)
+            cv2.imshow('u-hough',u_houghlines)
+#            cv2.imshow('obst_mask', obst_mask)
             cv2.imshow('maskonimg', mask_on_img)
-            cv2.imshow('disp_for_display', disp_np)
+            cv2.imshow('obstonimg', obst_on_img)
+
             cv2.imshow('V-disparity',V_disp)
+            cv2.imshow('U-disparity',U_disp)
 #            cv2.imshow("ZED", mat.get_data())
 #            cv2.imshow('refined_mask', refined_mask)
 
@@ -91,6 +119,8 @@ def main():
             settings(key, cam, runtime, mat)
         else:
             key = cv2.waitKey(5)
+    video_file.release()
+
     cv2.destroyAllWindows()
 
     cam.close()
